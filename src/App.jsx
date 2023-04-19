871fac4c208c41a7907d531d7b4895e6c991fa0a
PabloSan1997 <jos_pab_997@hotmali.com>
2023-04-18 22:06:48 -0600

    otro intento

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import "./styles/app.scss";
 import { Header } from './components/Header';
 import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { Menu } from './components/Menu';
 import { Stats } from './components/Stats';
 import { Contenedor } from './components/Contenedor';
 import { guardado } from './contexto/local';
