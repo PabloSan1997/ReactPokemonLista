c268c25a6f8c6a6efbd88e67dcd34267019a376f
PabloSan1997 <jos_pab_997@hotmali.com>
2023-04-18 21:46:02 -0600

    Se arregló error para produccion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import imagen from "../asserts/logo1.svg";
+import login from "../recursos/logo1.svg";
 import "../styles/header.scss";
 
 export function Header() {
   return (
     <header>
-        <img src={imagen} alt="logoPokebola" className='logoPokebola'/>
+        <img src={login} alt="logo" className='logoPokebola'/>
         <h1>Mi PokePagina</h1>
     </header>
   )
